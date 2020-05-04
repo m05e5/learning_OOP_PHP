3b91be1dc400269f9af270b0162fcd8dfa1faf07
m05e5 <calebmoses1111@gmail.com>
2020-05-04 11:19:14 +0200

    learning how to use classes

--- incudes/newclass.inc.php
+++ incudes/newclass.inc.php
@@ -1,3 +1,12 @@
 <?php
+class NewClass{
+
+	// properties and methods goes here
+	 public $info = "this is some info";
+} 
+
+$object = new NewClass;
+var_dump($object);
+
 
 ?>
---
+++ incudes/person.inc.php
@@ -0,0 +1,16 @@
+<?php
+class person{
+	private $first = "Daniel";
+	private $last = "Nielson";
+	private $age = "28";
+}
+
+class pet {
+	private function owner(){
+		$a = "Hi there!";
+
+		return $a;
+	}
+}
+
+?>
